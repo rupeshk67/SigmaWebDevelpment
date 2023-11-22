7e27b21a839bea5f912e94dc9388c94f2d32eb09
rupesh <rupeshk67@gmail.com>
2023-11-22 10:51:14 +0530

    scope notes

--- JS/03_basics/01_function.js
+++ JS/03_basics/01_function.js
@@ -9,8 +9,8 @@ function addTwoNumbers(number1,number2){
     // return result;
 
     // ====or=====
-    console.log("Rupesh"); //--> will be printed
-    return number1 + number2;
+    //console.log("Rupesh"); //--> will be printed
+    //return number1 + number2;
     console.log("Rupesh"); //--> will not be printed
     /*
     Note :- anything after "return" will not be executed.
@@ -19,17 +19,66 @@ function addTwoNumbers(number1,number2){
     */
 }
 
-const result = addTwoNumbers(3,5); //can store function return to a variable in JS
-console.log("Result :" ,result);
+//const result = addTwoNumbers(3,5); //can store function return to a variable in JS
+//console.log("Result :" ,result);
 
 // ==================================================
-function loginUserMessage(username){
-    if(!username /* or username ===undefined */){
-        console.log("Please enter a username");
-        return;
-    }
-    return `${username} just logged in`
+// function loginUserMessage(username){
+//     if(!username /* or username ===undefined */){
+//         console.log("Please enter a username");
+//         return;
+//     }
+//     return `${username} just logged in`
+// }
+// console.log(loginUserMessage("Rupesh"));
+// console.log(loginUserMessage(""));
+// console.log(loginUserMessage());
+
+//========Rest Operator in Function=========
+/* 
+Sometimes we are not sure how many parameters will
+come in function, in those scenario we can use 'Rest 
+Operator'.
+*/
+
+// function calculateCartPrice(...num1){
+//     return num1;
+// }
+
+// console.log(calculateCartPrice(200,300,400,500));
+
+// function calculateCartPrice(val1, val2, ...num1){
+//     //val1 and val2 will take the first 2 values and rest will be printed as array
+//     return num1;
+// }
+
+// console.log(calculateCartPrice(200,300,400,500));
+
+// ===========Function using object========
+
+const user ={
+    username:"rupeshk67",
+    price:200
 }
-console.log(loginUserMessage("Rupesh"));
-console.log(loginUserMessage(""));
-console.log(loginUserMessage());
+
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+//handleObject(user);
+// =====or=====
+//its not necessary that we have to create an object first then use it in function 
+//we can create the object at the time of calling function
+handleObject({
+    username : "Roy",
+    price : 300
+});
+
+// ========Array using function=======
+ const newArray =[400,500,600,700];
+
+ function returnSecondValue(getArray){
+    return getArray[1];
+ }
+ console.log(returnSecondValue(newArray));//created an array first and using in function call
+ console.log(returnSecondValue([600,700,800,900])); //direct passing array
