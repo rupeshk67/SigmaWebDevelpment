c9e0ae238330137c3b63275ea09af65079bcdefa
rupesh <rupeshk67@gmail.com>
2023-11-24 12:34:15 +0530

    control flow basics

---
+++ JS/04_control_flow/one.js
@@ -0,0 +1,19 @@
+//if
+
+const isUserLoggedIn = true;
+if(2 !=3){
+
+    console.log("That is true");
+}
+
+// comparison operators:-- <, >, <=, >=, ==, !=
+/*
+Note:-
+=================================================
+
+=   --> assignment operator
+==  --> equals operator
+=== --> stricktly checking equal operator
+        eg- 2==="2" will give false as data types are diff.(numb, string)
+=================================================
+*/
